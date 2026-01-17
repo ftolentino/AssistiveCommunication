829b53c8bdf3f8e12f8347d8a7bba06e7bd989ff
ftolentino <filmer.tolentino@gmail.com>
2026-01-16 21:36:43 -0800

    Setup typescript and nativewind for project

--- App.tsx
+++ App.tsx
@@ -1,16 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, Text, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import './global.css'
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
